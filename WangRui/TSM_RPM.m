c3b5fd3f03ea0c922f72547781c1bdcf89eafb98
ArayCHN <1172094131@qq.com>
2017-08-01 14:32:48 -0700

    changed to mac

--- WangRui/TSM_RPM.m
+++ WangRui/TSM_RPM.m
@@ -22,20 +22,10 @@ N = size(points,1); % total num of nodes on a rope
 clear points_Test_U
 T_W2U = transl((22.5 * 2.54 + 2.486 * 5) / 100, -2.486 * 2.5 / 100, 0) * trotz(-pi / 2); % transformation matrix, World to Kinect
 T_B2U = inv(si.ri{1}.T_B2W) * T_W2U; % transformation matrix, Robot Base to Kinect
-load('F:\WANGRUI\V4.0\data\Rope\Traj_Train_Straightening.mat'); % load the training data of trajectory
-load('F:\WANGRUI\V4.0\data\Rope\Bullet_Train_Straightening0.mat'); % load the training data of rope
+load('F:\WANGRUI\V4.0\data\Rope\TSM_RPM_rope.mat'); % load the training data of trajectory
+load('F:\WANGRUI\V4.0\data\Rope\Traj_Train_traj.mat'); % load the training data of rope
 
-% transform points from kinect frame to world frame
-% N1= size(points_U, 1)
-% N2= size(points_Test_U, 1)
-if N1 < N
-    N = N1;
-else
-    N1 = N;
-end
-points = points(1 : N, :);
-points_U = points_U(1 : N, :);
-%% select nodes on a rope
+%% transform points from kinect frame to world frame
 points_Test_U = [[points.X]', [points.Y]', [points.Z]']; % convert points(i).X,Y,Z to points_T_U(i, 1,2,3)
 points_U = [points_U, ones(size(points_U, 1), 1)];
 points_W = (T_W2U * points_U')';
@@ -47,44 +37,39 @@ points_Test_W = points_Test_W(:, 1 : 3);
 points_Test_U = points_Test_U(:, 1 : 3); % testing points transformed to world finished
 
 k = 2;
-WarpIndex = [1, 2];
+WarpIndex = [1, 2]; % WarpIndex can be 1, 2, or [1, 2]
 LTT_Data_Train.ReplayTime{1} = k * LTT_Data_Train.ReplayTime{1};
 LTT_Data_Train.ReplayTime{2} = k * LTT_Data_Train.ReplayTime{2};
 
-%% CPD-Warp the robot trajectory
-rigidCompensate = 0;  % ???
-[Z_Data_Test2, warp] = LTT_Warping_CPD(LTT_Data_Train, points_W(:, 1 : 2), points_Test_W(:, 1 : 2), si , '2D', WarpIndex, rigidCompensate);
-% Warping original rope to current rope finished!
+%% TPS-RPM-Warp the robot trajectory
+[LTT_Data_Test, warp] = TPS_RPM_Warp(LTT_Data_Train, points_W, points_Test_W, si, WarpIndex);
 
 disp('Please double check which robot''s motion needs to be warped!');
-fig2_handle = figure(2);
-set(fig2_handle, 'position', [962 562 958 434]);
+% Use the traj above to generate excecutable LTT_Data_Test series:
 
 % visualize the warping of the original training rope and the test rope
 fig3_handle = figure(3);
 set(fig3_handle, 'position', [962 562 958 434]);
 orig_fig = subplot(1,2,1); scatter(points_W(:, 1), points_W(:, 2), 'r*'); title('Train'); % plot the original rope 2-D shape
 warp_fig = subplot(1,2,2); scatter(points_Test_W(:, 1), points_Test_W(:, 2), 'r*'); title('Test'); % plot the test rope
-draw_grid([-0.5 0.8], [1 -0.7], warp, 20, orig_fig, warp_fig)
-subplot(orig_fig); axis equal; xlim([-0.5,1]); ylim([-0.7,0.8]); drawnow;
-subplot(warp_fig); axis equal; xlim([-0.5,1]); ylim([-0.7,0.8]); drawnow; % plote the grid
+% draw_grid([-0.5 0.8], [1 -0.7], warp, 20, orig_fig, warp_fig)
+% subplot(orig_fig); axis equal; xlim([-0.5,1]); ylim([-0.7,0.8]); drawnow;
+% subplot(warp_fig); axis equal; xlim([-0.5,1]); ylim([-0.7,0.8]); drawnow; % plote the grid
 
-% Run CFS, which displays an animation of fanuc robot following designed trajectory
+%% Run CFS, which displays an animation of fanuc robot following designed trajectory
 disp('======================================================================')
 % Remember to check collision if workpiece location is changed substantially!
 disp('======================================================================');
 fig1_handle = figure(2);
 set(fig1_handle, 'position', [962 42 958 434]);
 % save as LTT_Data_UCBtest for CFS check
-LTT_Data_UCBtest = Z_Data_Test2;
-save('F:\WANGRUI\V4.0\CFS_ver1.1\dataLTT\UCBTest.mat', 'LTT_Data_UCBtest');
+LTT_Data_UCBtest = LTT_Data_Test;
+save('F:\TeTang\V4.0\CFS_ver1.1\dataLTT\UCBTest.mat', 'LTT_Data_UCBtest');
 % CFS check
 CFS_Main
 input('Use CFS toolbox to check collision. Press any key to execute the motion!!!')
 
 
 %% Finally run the robot
-rigidCompensate = 0;
-[Z_Data_Test2, warp] = LTT_Warping_CPD(LTT_Data_Train, points_W(:,1:2), points_Test_W(:,1:2), si , '2D', WarpIndex, rigidCompensate);
 
-Traj_Download_Run(Z_Data_Test2, si, 'Download', 'Run');
+Traj_Download_Run(LTT_Data_Test, si, 'Download', 'Run');
